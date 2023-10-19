45b94ab8f4e95a7f9f617d244a7d04caf84bb303
kvo52@kvo1 <kvo.52@mail.ru>
2023-10-19 16:08:40 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿
+//Написать программу-калькулятор, вычисляющую выражения вида a + b, a - b, a / b, a * b,
+//введенные из командной строки, и выводящую результат выполнения на экран.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        char again = 'д';
+        while (again == 'д')
+        {
+            double a;
+            double b;
+            double total;
+            char oper;
+
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                total = a + b;
+                Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+            }
+
+            else if (oper == '-')
+            {
+                total = a - b;
+                Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+            }
+
+            else if (oper == '*')
+            {
+                total = a * b;
+                Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+            }
+
+            else if (oper == '/')
+            {
+                total = a / b;
+                Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор.");
+            }
+            Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
+            again = Convert.ToChar(Console.ReadLine());
+        }
+    }
+}
